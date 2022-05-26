bd13f29e1344cea813ff422cfc44e6863de03719
hnuruzada <90691865+hnuruzada@users.noreply.github.com>
2022-05-26 19:26:20 +0400

    Crud

---
+++ DAL/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using _23052022.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace _23052022.DAL
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Slider> Sliders { get; set; }
+        public DbSet<Setting> Settings { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _23052022.Models
+{
+    public class Product : BaseEntity
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+        public double DiscountedPrice { get; set; }
+        public string Image { get; set; }
+        public bool IsInStock { get; set; }
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+    }
+}
